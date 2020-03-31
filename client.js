8736b10df833dc8a02b07038a4f2472eed827aa0
Harun Ergul <harunergul@outlook.com>
2020-03-31 11:23:25 +0300

    Sending proxy error messages in the response

--- client.js
+++ client.js
@@ -1,12 +1,28 @@
-let httpProxy = require('http-proxy');
-let http = require('http');
+let httpProxy = require("http-proxy");
+let http = require("http");
 
-let proxy = httpProxy.createProxyServer({ target: "http://192.168.1.36:9005" });
+let proxy = httpProxy.createProxyServer({});
 
-let server = http.createServer(function (request, response) {
-    console.log("Request arrived");
-    proxy.web(request, response, { target: 'http://192.168.1.36:9005' })
+proxy.on("error", function(err, req, res) {
+  console.log(err);
+  res.writeHead(500, {
+    "Content-Type": "text/plain"
+  });
+
+  res.write("\nERROR: " + err.name);
+  res.write("\nERROR: " + err.stack);
+  res.end("\nERROR MESSAGE: " + err.message);
 });
 
-console.log("listening on port 80");
+let server = http.createServer(function(request, response) {
+  console.log("Request arrived");
+  proxy.web(request, response, { target: "http://192.168.1.37:9005" });
+});
+
+server.on("uncaughtException", function(e) {
+  // Handle your error here
+  console.log(e);
+});
+
+console.log("listening on port 8080");
 server.listen(80);
